ef11ad8bdbdce7cea261c9818732af5e815ba887
unknown <m.k.kepinska@gmail.com>
2020-03-23 18:40:36 +0100

    add timer

--- src/App.js
+++ src/App.js
@@ -8,23 +8,67 @@ class App extends Component {
     super(props);
     this.state = {
       step: "set",
-      challenges: []
-    }
+      challenges: [],
+      leftParts: {
+        hours: "00",
+        minutes: "00",
+        seconds: "00"
+      }
+    };
   }
 
-  addChallenge = (challenge) => {
-    this.setState({challenges: [...this.state.challenges, challenge]});
-  }
+  addChallenge = challenge => {
+    this.setState({ challenges: [...this.state.challenges, challenge] });
+  };
 
-  startingTheGame = (isStarted) => {
-    this.setState({step: "start"});
-  }
-
+  startingTheGame = isStarted => {
+    this.setState({ step: "start" });
+    setInterval(() => {
+      const initDate = new Date();
+      const initYear = initDate.getFullYear();
+      const initMonth = `0${initDate.getMonth() + 1}`;
+      const initDay = initDate.getDate();
+      const nextDay = `${initYear}-${initMonth}-${initDay + 1}`;
+      const nextDate = new Date(nextDay).setHours(0);
+      const left = nextDate - initDate;
+      this.setState({
+        leftParts: {
+          hours:
+            Math.floor((left / (1000 * 60 * 60)) % 24) > 9
+              ? Math.floor((left / (1000 * 60 * 60)) % 24)
+              : `0${Math.floor((left / (1000 * 60 * 60)) % 24)}`,
+          minutes:
+            Math.floor((left / 1000 / 60) % 60) > 9
+              ? Math.floor((left / 1000 / 60) % 60)
+              : `0${Math.floor((left / 1000 / 60) % 60)}`,
+          seconds:
+            Math.floor((left / 1000) % 60) > 9
+              ? Math.floor((left / 1000) % 60)
+              : `0${Math.floor((left / 1000) % 60)}`
+        }
+      });
+    }, 1000);
+  };
 
   render() {
-    if (this.state.step === "set") { return <Set addChallenge={this.addChallenge} challenges={this.state.challenges} startingTheGame={this.startingTheGame}/>}
-    else if (this.state.step === "start") { return <Start challenges={this.state.challenges}/>}
-    else { return <Result />}
+    if (this.state.step === "set") {
+      return (
+        <Set
+          addChallenge={this.addChallenge}
+          challenges={this.state.challenges}
+          startingTheGame={this.startingTheGame}
+        />
+      );
+    } else if (this.state.step === "start") {
+      return (
+        <Start
+          challenges={this.state.challenges}
+          leftParts={this.state.leftParts}
+        />
+      );
+    } else {
+      return <Result />;
+    }
   }
 }
 
--- src/Components/Start/Start.js
+++ src/Components/Start/Start.js
@@ -2,11 +2,16 @@ import React from "react";
 import Challenge from "../Challenge/Challenge";
 import "./Start.css";
 
-const Start = (props) => {
+const Start = props => {
+
+
   return (
     <div className="start">
       <h1>Let's do it!</h1>
-      {props.challenges.map(c => <Challenge challenge={c}/>)}
+      {props.challenges.map(c => (
+        <Challenge challenge={c} />
+      ))}
+      {props.leftParts.hours} : {props.leftParts.minutes} : {props.leftParts.seconds}
     </div>
   );
 };
