684c643685da5603a2a0a69517a15a65e2535a74
tynakpinska <m.k.kepinska@gmail.com>
2020-04-06 21:59:54 +0200

    add events for logging in

--- src/Components/Routes/LogIn/LogIn.js
+++ src/Components/Routes/LogIn/LogIn.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import "./LogIn.css";
 
-function LogIn(props) {
-  return (
-    <div className="container">
+class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  handleUsernameChange = e => {
+    this.setState({username: e.target.value});
+  }
+
+  handlePasswordChange = e => {
+    this.setState({password: e.target.value});
+  }
+
+
+  handleSubmit = (e) => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      }),
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
         <h1>Log in</h1>
-        <form className ="login">
-            <label htmlFor="username" name="username">Username</label>
-            <input type="text" name="username"></input>
-            <label htmlFor="password" name="password">Password</label>
-            <input type="password" name="password"></input>
-            <input type="submit" value="Log in"></input>
-        </form>
-    </div>
-  );
+        <div className="login">
+          <label htmlFor="username" name="username">
+            Username
+          </label>
+          <input type="text" name="username" onChange={this.handleUsernameChange}></input>
+          <label htmlFor="password" name="password">
+            Password
+          </label>
+          <input type="password" name="password" onChange={this.handlePasswordChange}></input>
+          <input type="submit" value="Log in" onClick={this.handleSubmit}></input>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LogIn;
