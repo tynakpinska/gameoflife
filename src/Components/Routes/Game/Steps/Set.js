629ad9ec8d7470a471f099465b896df47f2fb206
tynakpinska <m.k.kepinska@gmail.com>
2020-04-19 12:39:25 +0200

    make adding challenges by clicking button working

--- src/Components/Routes/Game/Steps/Set.js
+++ src/Components/Routes/Game/Steps/Set.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import { v4 as uuidv4 } from "uuid"; // create random keys
 import ChallengesList from "../ChallengesList";
@@ -8,7 +8,7 @@ import {
   addChallenge,
   removeChallenge,
   setStep,
-  editChallenge
+  editChallenge,
 } from "../../../../redux/actions";
 
 const mapStateToProps = ({ challenges, step, route, user }) => {
@@ -16,7 +16,7 @@ const mapStateToProps = ({ challenges, step, route, user }) => {
     route,
     step,
     challenges,
-    user
+    user,
   };
 };
 
@@ -34,6 +34,7 @@ class Set extends Component {
     super(props);
     this.state = {
       startFailed: false,
+      inputValue: "",
     };
   }
 
@@ -46,21 +47,22 @@ class Set extends Component {
       e.target.value = "";
       this.setState({ startFailed: false });
       if (user.username) {
-      fetch("http://localhost:3000/saveChallenge", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          user,
-          challenge,
-          key: id,
-          date: new Date(),
-          isDone: false,
-        }),
-      })
-        .then(resp => resp.json())
-        .then(resp => console.log(resp))
-        .catch(err => console.log(err, "Unable to save challenge"));
-    }}
+        fetch("http://localhost:3000/saveChallenge", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            user,
+            challenge,
+            key: id,
+            date: new Date(),
+            isDone: false,
+          }),
+        })
+          .then(resp => resp.json())
+          .then(resp => console.log(resp))
+          .catch(err => console.log(err, "Unable to save challenge"));
+      }
+    }
   };
 
   handleStartClick = () => {
@@ -69,9 +71,45 @@ class Set extends Component {
       : this.setState({ startFailed: true });
   };
 
-  render(
-    { user } = this.props
-  ) {
+  handleOnMouseOver = e => {
+    e.target.style.color = "black";
+  };
+
+  handleOnMouseLeave = e => {
+    e.target.style.color = "inherit";
+  };
+
+  handleInputChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleButtonClick = e => {
+    const challenge = this.state.inputValue;
+    if (challenge !== "") {
+      const user = this.props.user;
+      const id = uuidv4();
+      this.props.addChallenge(challenge, id);
+      document.querySelector(".typeChall").value = "";
+      if (user.username) {
+        fetch("http://localhost:3000/saveChallenge", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            user,
+            challenge,
+            key: id,
+            date: new Date(),
+            isDone: false,
+          }),
+        })
+          .then(resp => resp.json())
+          .then(resp => console.log(resp))
+          .catch(err => console.log(err, "Unable to save challenge"));
+      }
+    }
+  };
+
+  render({ user } = this.props) {
     return (
       <div className="container">
         <h1>
@@ -79,12 +117,21 @@ class Set extends Component {
             ? `${user.username}, what are you playing today?`
             : "What are you playing today?"}
         </h1>
-        <input
-          className="typeChall"
-          type="text"
-          placeholder="Set challenge and press enter"
-          onKeyUp={this.handleEnter}
-        ></input>
+        <div className="inputContainer">
+          <input
+            className="typeChall"
+            type="text"
+            placeholder="e.g. learn javascript"
+            onKeyUp={this.handleEnter}
+            onChange={this.handleInputChange}
+          ></input>
+          <i
+            onMouseOver={this.handleOnMouseOver}
+            onMouseLeave={this.handleOnMouseLeave}
+            onClick={this.handleButtonClick}
+            className="addChallButton demo-icon icon-plus-circled"
+          ></i>
+        </div>
         <ChallengesList />
         <button onClick={this.handleStartClick}>Start the game!</button>
         <p
