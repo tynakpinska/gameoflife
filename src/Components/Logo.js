a02ceefb23209572aae9aabe58f1ba5d395f40fd
tynakpinska <m.k.kepinska@gmail.com>
2020-05-01 21:39:12 +0200

    change styles

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,7 +3,10 @@ import React from "react";
 const Logo = props => {
   return (
     <div className="logo">
-      <h1 onClick={props.onClick}>Game of Life</h1>
+      <div className="name"><h1 onClick={props.onClick}>Game of Life</h1></div>
+      <div className="dot1"></div>
+      <div className="dot2"></div>
+      <div className="dot3"></div>
     </div>
   );
 };
--- src/Components/Routes/Profile.js
+++ src/Components/Routes/Profile.js
@@ -2,21 +2,30 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import avatar from "../../img/user.png";
 
-const mapStateToProps = ({user}) => {
-  return {user};
+const mapStateToProps = ({ user }) => {
+  return { user };
 };
 
 class Profile extends Component {
-  render({user} = this.props) {
+  render({ user } = this.props) {
     return (
       <div className="container profile">
-        <h1>{user.username}</h1>
+        <h2>{user.username}</h2>
         <img src={avatar} alt="avatar" />
-        <p>Change profile image</p>
-        <div><h4>Streak</h4></div>
-        <div><h4>State of mind</h4></div>
-        <div><h4>Body shape</h4></div>
-        <div><h4>Bank balance</h4></div>
+        <label for="img"><i className="demo-icon icon-upload"></i>Change profile picture</label>
+        <input type="file" id="img" name="img" accept="image/*" />
+        <div>
+          <h4>Streak</h4>
+        </div>
+        <div>
+          <h4>State of mind</h4>
+        </div>
+        <div>
+          <h4>Body shape</h4>
+        </div>
+        <div>
+          <h4>Bank balance</h4>
+        </div>
       </div>
     );
   }
