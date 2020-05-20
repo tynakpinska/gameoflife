8b8ce2e617b532eb7de0a6a03889643dab5fc5d9
tynakpinska <m.k.kepinska@gmail.com>
2020-05-20 20:46:25 +0200

    slider with react-transition-group

--- src/Components/Footer/Slider/Slider.js
+++ src/Components/Footer/Slider/Slider.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { info, slider, textSpan, icon } from "./Slider.module.css";
-import { CSSTransitionGroup } from 'react-transition-group';
+import styles, { info, slider, textSpan, icon, iconLeft, iconRight, wrapper } from "./Slider.module.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(1);
@@ -22,86 +22,90 @@ const Slider = () => {
         {currentSlide !== 1 ? (
           <span
             onClick={handleChangeSlide}
-            className={`demo-icon icon-left-open-big ${icon}`}
+            className={`demo-icon icon-left-open-big  ${icon} ${iconLeft}`}
           ></span>
         ) : (
           <div style={{ flex: "1" }}></div>
         )}
-        <CSSTransitionGroup
-        transitionName="carousel"
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={300}
-        key={currentSlide}>
-            {{
-              2: (
-                <>
-                  <p>
-                    <span className={textSpan}>First</span>, decide what do you
-                    want your life to be like in three main areas (according to{" "}
-                    <a
-                      href="https://zenjaskiniowca.pl/o-mnie/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Rafał Mazur
-                    </a>
-                    ):{" "}
-                  </p>
-                  <ol>
-                    <li>body shape</li>
-                    <li>state of mind</li>
-                    <li>bank balance</li>
-                  </ol>
-                </>
-              ),
-              3: (
+        
+      <TransitionGroup>
+            <CSSTransition
+              key={currentSlide}
+              timeout={{ enter: 1000, exit: 1000 }}
+              classNames={{...styles}}
+            >
+              <div className={wrapper}>
+        {{
+          2: (
+              <div>
                 <p>
-                  <span className={textSpan}>Second</span>, every morning think
-                  of three to five todos that will significantly close you to
-                  your dreams defined in the previous step. Add tasks to your
-                  power list by clicking Enter.
+                  <span className={textSpan}>First</span>, decide what do you
+                  want your life to be like in three main areas (according to{" "}
+                  <a
+                    href="https://zenjaskiniowca.pl/o-mnie/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Rafał Mazur
+                  </a>
+                  ):{" "}
                 </p>
-              ),
-              4: (
-                <p>
-                  <span className={textSpan}>Finally</span>, start the game and
-                  smash every todo! The app shows how much time till the end of
-                  the day you have left. Consider it a game. If you do all the
-                  challenges - you won the game. If you don't - you lost.
-                </p>
-              ),
-              5: (
-                <p>
-                  <b className={textSpan}>Additionaly</b>, after a busy day, see
-                  your stats. Admire how hard have you worked during a week, a
-                  year, and see how far it got you. Try it out and see it for
-                  yourself!
-                </p>
-              ),
-            }[currentSlide] || (
-              <p>
-                The goal of this app is to help you achieve success by
-                motivating you to do something small, that closes you to
-                fulfilling your dreams, each day. It was inspired by{" "}
-                <a
-                  href="https://zenjaskiniowca.pl/jak-wyrobic-nawyk-wygrywania/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Zen Jaskiniowca
-                </a>{" "}
-                podcast, that really helped me see how important everyday
-                activities are and how powerful doing little tasks can make you
-                feel. After some time you'll build a habit of winning and you'll
-                start to feel extremely effective.
-              </p>
-            )}
-        </CSSTransitionGroup>
+                <ol>
+                  <li>body shape</li>
+                  <li>state of mind</li>
+                  <li>bank balance</li>
+                </ol>
+              </div>
+          ),
+          3: (
+            <p>
+              <span className={textSpan}>Second</span>, every morning think of
+              three to five todos that will significantly close you to your
+              dreams defined in the previous step. Add tasks to your power list
+              by clicking Enter.
+            </p>
+          ),
+          4: (
+            <p>
+              <span className={textSpan}>Finally</span>, start the game and
+              smash every todo! The app shows how much time till the end of the
+              day you have left. Consider it a game. If you do all the
+              challenges - you won the game. If you don't - you lost.
+            </p>
+          ),
+          5: (
+            <p>
+              <b className={textSpan}>Additionaly</b>, after a busy day, see
+              your stats. Admire how hard have you worked during a week, a year,
+              and see how far it got you. Try it out and see it for yourself!
+            </p>
+          ),
+        }[currentSlide] || (
+          <p>
+            The goal of this app is to help you achieve success by motivating
+            you to do something small, that closes you to fulfilling your
+            dreams, each day. It was inspired by{" "}
+            <a
+              href="https://zenjaskiniowca.pl/jak-wyrobic-nawyk-wygrywania/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Zen Jaskiniowca
+            </a>{" "}
+            podcast, that really helped me see how important everyday activities
+            are and how powerful doing little tasks can make you feel. After
+            some time you'll build a habit of winning and you'll start to feel
+            extremely effective.
+          </p>
+        )}
+        </div>
+        </CSSTransition>
+        </TransitionGroup>
         {currentSlide !== 5 ? (
           <div style={{ flex: "1" }}>
             <span
               onClick={handleChangeSlide}
-              className={`demo-icon icon-right-open-big ${icon}`}
+              className={`demo-icon icon-right-open-big ${icon} ${iconRight}`}
             ></span>
           </div>
         ) : (
