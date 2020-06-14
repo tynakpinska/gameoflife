6335f62ea827009220a7c7fbd7bdfbc2e6acfc71
tynakpinska <m.k.kepinska@gmail.com>
2020-06-14 18:44:57 +0200

    remove dots, polish profile page

--- src/Components/Visual/Logo.js
+++ src/Components/Visual/Logo.js
@@ -5,9 +5,6 @@ const Logo = props => {
   return (
     <div className={styles.logo}>
       <div className={styles.name}><h1 onClick={props.onClick}>Game of Life</h1></div>
-      <div className={styles.dot1}></div>
-      <div className={styles.dot2}></div>
-      <div className={styles.dot3}></div>
     </div>
   );
 };
