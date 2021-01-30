830f1938791c4101a72175f31dc7573b9aba42c7
tynakpinska <m.k.kepinska@gmail.com>
2021-01-30 16:30:53 +0100

    start fixing mobile view

--- src/Components/Routes/Game/ChallengesList.js
+++ src/Components/Routes/Game/ChallengesList.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import SimpleBar from "simplebar-react";
 import styles from "./ChallengesList.module.css";
-import "simplebar/dist/simplebar.min.css";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Challenge from "./Challenge";
@@ -22,32 +20,23 @@ const mapDispatchToProps = dispatch => {
 };
 
 const ChallengesList = ({ challenges }) => {
-
   return (
-    <SimpleBar
-      style={{
-        maxHeight: "55%",
-        width: "80%",
-        margin: "auto",
-      }}
-    >
-      <TransitionGroup>
-        {challenges.map(c => (
-          <CSSTransition
+    <TransitionGroup>
+      {challenges.map(c => (
+        <CSSTransition
+          key={c.key}
+          timeout={{ enter: 1000, exit: 1000 }}
+          classNames={{ ...styles }}
+        >
+          <Challenge
+            challenge={c.challenge}
             key={c.key}
-            timeout={{ enter: 1000, exit: 1000 }}
-            classNames={{ ...styles }}
-          >
-            <Challenge
-              challenge={c.challenge}
-              key={c.key}
-              id={c.key}
-              isDone={c.isDone}
-            />
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
-    </SimpleBar>
+            id={c.key}
+            isDone={c.isDone}
+          />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   );
 };
 
